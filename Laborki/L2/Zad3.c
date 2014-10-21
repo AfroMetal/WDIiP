50da6d8094380577f9227e94b2cc1dd107a014c7
AfroMetal <radek.kwlsk@gmail.com>
2014-10-21 17:40:47 +0200

    Create Zad3.c

---
+++ Laborki/L2/Zad3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  double n=1.0,a;
+  for(int i=1; i<=1000; i++)
+  {
+    a = pow(i,(double)1/(double)1000);
+    n *= a;
+  }
+  printf("%f\n", n);
+
+  return 0;
+}
